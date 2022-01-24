9c6c2eff018776b1676481c44f53c25519d2d388
Jim Wang <jim.wang.nj@gmail.com>
2022-01-24 14:12:45 -0500

    Moving the calulation logic to calculator.py

--- calculator.py
+++ calculator.py
@@ -22,22 +22,26 @@ def _get_from_etherscan (data_type):
 
     return df
 
-def _get_used_price_from_keepa (product_id, api_key):
+def _get_gpu_prices_from_keepa (product_id, api_key):
+    def clean_up(raw_df):
+        # clean up the data to make it look like other time series
+        raw_df['Date(UTC)'] = pd.to_datetime(raw_df.index).date
+        raw_df.dropna(inplace=True)
+        clean_series = raw_df.groupby(['Date(UTC)'])['value'].mean()
+        clean_df = clean_series.reset_index()
+        clean_df.set_index('Date(UTC)',inplace=True)
+        return clean_df
+
     api = keepa.Keepa(api_key)
     #products = api.query('B06XZQMMHJ')
     products = api.query(product_id)
-    df = pd.DataFrame(products[0]['data']['df_USED'])
-    
-    df['Date(UTC)'] = pd.to_datetime(df.index).date
+    df_new = clean_up(pd.DataFrame(products[0]['data']['df_NEW']))
+    df_new.rename(columns={'value':'new_gpu_price'},inplace=True)
 
-    # clean up the data to make it look like other time series
-    df.dropna(inplace=True)
-    clean_series = df.groupby(['Date(UTC)'])['value'].mean()
-    clean_df = clean_series.reset_index()
-    clean_df.set_index('Date(UTC)',inplace=True)
-    clean_df.rename(columns={'value':'Value'},inplace=True)
+    df_used = clean_up(pd.DataFrame(products[0]['data']['df_USED']))
+    df_used.rename(columns={'value':'used_gpu_price'},inplace=True)
 
-    return clean_df
+    return df_new, df_used
  
 def _create_blank_df ():
     number_days = 1500
@@ -59,8 +63,8 @@ def _prepare_calc_data(keepa_api_key):
     ep_df.rename (columns={'Value': 'ether_price'}, inplace=True)
     df = df.merge(ep_df, left_index=True, right_index=True, how='left')
 
-    up_df = _get_used_price_from_keepa ('B06XZQMMHJ', keepa_api_key)
-    up_df.rename (columns={'Value': 'gpu_price'}, inplace=True)
+    new_df, up_df = _get_gpu_prices_from_keepa ('B06XZQMMHJ', keepa_api_key)
+    df = df.merge(new_df, left_index=True, right_index=True, how='left')
     df = df.merge(up_df, left_index=True, right_index=True, how='left')
 
     return df
@@ -71,12 +75,77 @@ def _add_calculated_cols (df):
     # convert to dollar
     df['dollar_reward_per_ghps'] = df['reward_per_ghps'] * df['ether_price']
     #fill the gaps in gpu price
-    df['gpu_price'].interpolate(inplace=True)
-    df['gpu_price'].fillna(method='backfill', inplace=True)
-    
+    df['used_gpu_price'].interpolate(inplace=True)
+    df['used_gpu_price'].fillna(method='backfill', inplace=True)
+    df['new_gpu_price'].interpolate(inplace=True)
+    df['new_gpu_price'].fillna(method='backfill', inplace=True)
+
 def calc_matrix(keepa_api_key):
     ''' A matrix with enough data for calculation
     '''
     df = _prepare_calc_data(keepa_api_key)
     _add_calculated_cols(df)
     return df
+
+def calc (df, start_date, end_date, gpu_giga_hashrate, rig_price=None, electricity_price=0.1, gpu_number=8, 
+chassis_cost=500, rig_kilowatt=1, buyback_multiplier=1):
+    '''
+     Returns calculation results
+        Parameters:
+            df (DataFrame): A DF object with historical data populated.
+            start_date (datetime): Mining starts on this date
+            end_date (datetime): Ming ends on this date
+            gpu_giga_hashrate: capable of performing number of hashes per hour.
+            gpu_number: number of GPU boards.
+            rig_price (float): The price of the rig.
+            electricity_price: In dollar killo Wattes per hour.
+            chassis_cost (float): The cost of the chassis that hosts the GPU cards
+            rig_kilowatt (float): The power consumption in Kilowatt 
+            buyback_multiplier: This multiplier will be applied to calculated buyback price based on used GPU card price.
+        Returns:
+            calulation results.
+            
+            Here is an example:
+            "rig_price":2958.248888888889
+            "rig_buyback_price":5100
+            "start_ether_price":730.6
+            "end_ether_price":2541.6
+            "ether_mined":2.214013075929777
+            "ether_mined_dollar":5574.053511512345
+            "start_date":"datetime.date(2021, 1, 1)"
+            "end_date":"datetime.date(2022, 1, 23)"
+            "electricity_price":0.1
+            "total_electricity_cost":931.2
+            "rows_in_range": dataframe only contains the rows between the start date and end date.  Can
+                             be used for creating charts.
+    '''
+    start = pd.to_datetime(start_date)
+    end = pd.to_datetime(end_date)
+    num_hours = (end - start).total_seconds()//3600 + 24
+
+    start_ether_price = df.at[start, "ether_price"]
+    end_ether_price = df.at[end, "ether_price"]
+
+    if not rig_price:
+        rig_price = gpu_number * df.at[start, "new_gpu_price"] + chassis_cost
+
+    total_electricity_cost = electricity_price * num_hours * rig_kilowatt
+    rows_in_range = df.loc[(df.index >= start) & (df.index <= end)]
+    ether_mined_dollar = rows_in_range['dollar_reward_per_ghps'].sum()*gpu_giga_hashrate
+    ether_mined = rows_in_range['reward_per_ghps'].sum()*gpu_giga_hashrate
+
+    rig_buyback_price = (gpu_number * df.at[end, "used_gpu_price"] + chassis_cost)*buyback_multiplier
+
+    return {
+        "rig_price" : rig_price,
+        "rig_buyback_price" : rig_buyback_price,
+        "start_ether_price" : start_ether_price,
+        "end_ether_price" : end_ether_price,
+        "ether_mined" : ether_mined,
+        "ether_mined_dollar" : ether_mined_dollar,
+        "start_date" : start.date(),
+        "end_date" : end.date(),
+        "electricity_price" : electricity_price,
+        "total_electricity_cost" : total_electricity_cost,
+        "rows_in_range" : rows_in_range
+    }
--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import re
 from tokenize import String
 import streamlit as st
 from datetime import datetime, date, timedelta
-from calculator import calc_matrix
+from calculator import calc_matrix,calc
 
 import pandas as pd
 
@@ -10,16 +10,14 @@ import pandas as pd
 def calculate_matrix(today):
     return calc_matrix(st.secrets.KEEPA_API_KEY)
 
-
 today = datetime.utcnow().date()
 #st.write(f'today(UTC) : {today}')
 
 c_df = calculate_matrix(today)
-#st.write(c_df)
+st.write(c_df)
 
 min_date = c_df.index.min()
 max_date = c_df.index.max()
-
 
 gpu_selection:String = st.sidebar.selectbox("GPU Cards:", ("8 x NVIDIA GeForce RTX 3090", "8 x AMD Radeon RX 580"),
                                 help="Select the number of GUPs and model that match your configuration.")
@@ -31,91 +29,74 @@ gpu_number = float(gpu_number_str)
 raw_start_date = st.sidebar.date_input("Mining Starts on:",
                                    value=date(2021,1,1), min_value=min_date, max_value=max_date, 
                                    help="Select a date in the past.")
-start_date = pd.to_datetime(raw_start_date)
 
 raw_end_date = st.sidebar.date_input("Mining Ends on:",
                                    value=max_date, min_value=min_date, max_value=max_date, 
                                    help="Select a date in the past.")
-end_date = pd.to_datetime(raw_end_date)
 
 len_in_days = (raw_end_date - raw_start_date).days + 1
 
-#len_in_days = st.sidebar.number_input(
-#    "Number of Days of Mining:", value=360, min_value=0, max_value=1080, step = 30)
-
-
-electricity_cost = st.sidebar.text_input(
+electricity_cost_text = st.sidebar.text_input(
     "Electricity Cost:", value="10 cents per kWh")
 
-price_paid = st.sidebar.text_input(
-    "Non GPU Hardware Cost:", value="500",help="Cost of the rigging peripherals, e.g chassis")
+#price_paid_text = st.sidebar.text_input(
+#    "Non GPU Hardware Cost:", value="500",help="Cost of the rigging peripherals, e.g chassis")
 #other_paid = st.sidebar.text_input("Other Cost Adjustment:", help="Put value in USD to reflect other initial investment in the mining rig.")
 
 strategies = ("Mine & Hold", "Mine & Sell", "Mine & Sell 50%", "Buy BTC", "Buy ETH")
 
-#strategy = st.sidebar.selectbox("Strategy:", strategies)
+def show_results(df):
+    r = calc(df, raw_start_date, raw_end_date, 0.24)
+    r
+    # results = f"""
+    # ## Mining Profits from {raw_start_date} to {raw_end_date}
+    # ### Length: {len_in_days} Days
 
-gpu_price = 600.0
-#end_date = start_date + timedelta(days=len_in_days)
+    # ### Estimated Initial Investment (based on used GPU market price)
+    # |  | Amount USD | Amount in ETH |
+    # | --- | --- | --- |
+    # | {gpu_selection} | {r[rig_price_start} | {rig_price_start_ether} |
+ 
+    # ### Returns by Scenarios
+    # | Scenarios | Return in USD | Return in ETH | % Return on Investment |
+    # | --- | --- | --- | --- |
+    # | {strategies[0]} | 2000 | 2.0 | 140% |
+    # | {strategies[1]} | 2000 | 2.0 | 140% |
+    # | {strategies[2]} | 2000 | 2.0 | 140% |
+    # | {strategies[3]} | 2000 | 2.0 | 140% |
+    # | {strategies[4]} | 2000 | 2.0 | 140% |
 
-results = f"""
-## Mining Profits from {start_date} to {end_date}
-### Length: {len_in_days} Days
+    # ### {strategies[0]}
+    # | | Amount USD | Amount in ETH |
+    # | --- | --- | --- |
+    # | Ethereum | 6000 | 2.0 |
+    # | USD | 0 | 0 |
+    # | Rig Buyout | 3,800 | |
+    # | __Total__ | 3,000 | |
+    # | ROI | 150% | |
 
-### Initial Investment
-|  | Amount USD | Amount in ETH |
-| --- | --- | --- |
-| {gpu_selection} | {gpu_price * gpu_number} | 2.0 |
-| Non GPU Hardware | {price_paid} | 0.5 |
-| __Total__ | 20,000 | 2.5 |
+    # ### {strategies[1]}
+    # | | Amount USD | Amount in ETH |
+    # | --- | --- | --- |
+    # | Ethereum | 6000 | 2.0 |
+    # | USD | 0 | 0 |
+    # | Rig Buyout | 3,800 | |
+    # | __Total__ | 3,000 | |
+    # | ROI | 150% | |
 
-### Returns by Scenarios
-| Scenarios | Return in USD | Return in ETH | % Return on Investment |
-| --- | --- | --- | --- |
-| {strategies[0]} | 2000 | 2.0 | 140% |
-| {strategies[1]} | 2000 | 2.0 | 140% |
-| {strategies[2]} | 2000 | 2.0 | 140% |
-| {strategies[3]} | 2000 | 2.0 | 140% |
-| {strategies[4]} | 2000 | 2.0 | 140% |
-
-### {strategies[0]}
-| | Amount USD | Amount in ETH |
-| --- | --- | --- |
-| Ethereum | 6000 | 2.0 |
-| USD | 0 | 0 |
-| Rig Buyout | 3,800 | |
-| __Total__ | 3,000 | |
-| ROI | 150% | |
-
-### {strategies[1]}
-| | Amount USD | Amount in ETH |
-| --- | --- | --- |
-| Ethereum | 6000 | 2.0 |
-| USD | 0 | 0 |
-| Rig Buyout | 3,800 | |
-| __Total__ | 3,000 | |
-| ROI | 150% | |
-
-### Returns Comparison Chart
-"""
+    # ### Returns Comparison Chart
+    # """
+    # st.markdown(results)
+    # st.header("Historical Ether Price")
+    # st.line_chart(rows_in_range ['ether_price'], width=800, use_container_width=False)
+    # st.header("Daily Reward in Dollar per GH per Second")
+    # st.line_chart(rows_in_range ['dollar_reward_per_ghps'], width=800, use_container_width=False)
+    # st.header("Daily Reward in Ether per GH per Second")
+    # st.line_chart(rows_in_range ['reward_per_ghps'], width=800, use_container_width=False)
+    # st.header("Historical RX850 GPU price")
+    # st.line_chart(rows_in_range ['gpu_price'], width=800, use_container_width=False)
 
 if st.sidebar.button("Submit"):
-    gpu_price_start = c_df['gpu_price'][start_date]
-    gpu_price_end = c_df['gpu_price'][end_date]
-    gpu_hash_rate_ghps = 0.24
-
-    rows_in_range = c_df.loc[(c_df.index >= start_date) & (c_df.index <= end_date)]
-    ether_mined = rows_in_range['reward_per_ghps']*rows_in_range['ether_price'].sum()*gpu_hash_rate_ghps
-    
-    #st.markdown(results)
-    st.header("Historical Ether Price")
-    st.line_chart(rows_in_range ['ether_price'], width=800, use_container_width=False)
-    st.header("Daily Reward in Dollar per GH per Second")
-    st.line_chart(rows_in_range ['dollar_reward_per_ghps'], width=800, use_container_width=False)
-    st.header("Daily Reward in Ether per GH per Second")
-    st.line_chart(rows_in_range ['reward_per_ghps'], width=800, use_container_width=False)
-    st.header("Historical RX850 GPU price")
-    st.line_chart(rows_in_range ['gpu_price'], width=800, use_container_width=False)
+    show_results(c_df)
 else:
     st.error("Use the left panel to input your rigging info and click on Sumbit button to see results.")
-
